d34fdcd02dca3326e5b2896cc218f711ecdfb852
Adham <adhamnotheare@gmail.com>
2022-12-27 18:17:27 +0530

    Added spdlog

--- BackupEngine/src/BEngine/EntryPoint.h
+++ BackupEngine/src/BEngine/EntryPoint.h
@@ -6,6 +6,11 @@ extern BEngine::Application* BEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	BEngine::Log::Init();
+
+	EN_CORE_WARN("Initialized Log!");
+	EN_TRACE("Initialized Log!");
+
 	auto app = BEngine::CreateApplication();
 	app->Run();
 	delete app;
--- BackupEngine/src/BackupEngine.h
+++ BackupEngine/src/BackupEngine.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "BEngine/Application.h"
+#include "BEngine/Log.h"
 
 /////////////////////////Entry Point///////////////////////////////
 #include "BEngine/EntryPoint.h"
